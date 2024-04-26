09e12395f82c15fd4a11f1308228618f1be763c6
Anders Porsbo <apor@tv2.dk>
2024-04-26 11:51:17 +0200

    upgrading to aft 1.12.2

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+locals {
+  github_org  = "apor-tv2"
+}
 
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  source                                          = "aws-ia/control_tower_account_factory/aws"
+  version                                         = " = 1.12.2"
+
+
+
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
   audit_account_id            = var.audit_account_id
@@ -10,10 +15,23 @@ module "aft" {
   ct_home_region              = var.ct_home_region
   tf_backend_secondary_region = var.tf_backend_secondary_region
 
-  aft_vpc_endpoints = false
+
+  # Optional Parameters
+  terraform_distribution                          = "oss"  
+
+  # Optional Feature Flags
+  aft_feature_delete_default_vpcs_enabled         = true
+  aft_feature_cloudtrail_data_events              = false
+  aft_feature_enterprise_support                  = true
+  aft_vpc_endpoints                               = false
+
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "${var.github_username}/sbx-terraform-aft-account-request"
-  account_provisioning_customizations_repo_name = "${var.github_username}/sbx-terraform-aft-account-provisioning-customizations"
-  global_customizations_repo_name               = "${var.github_username}/sbx-terraform-aft-global-customizations"
-  account_customizations_repo_name              = "${var.github_username}/sbx-terraform-aft-account-customizations"
+
+
+
+
+  account_request_repo_name                     = "${local.github_org}/sbx-terraform-aft-account-request"
+  account_provisioning_customizations_repo_name = "${local.github_org}/sbx-terraform-aft-account-provisioning-customizations"
+  global_customizations_repo_name               = "${local.github_org}/sbx-terraform-aft-global-customizations"
+  account_customizations_repo_name              = "${local.github_org}/sbx-terraform-aft-account-customizations"
 }
